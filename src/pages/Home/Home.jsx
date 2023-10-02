eec46d78857b1e7afc1af81f46baa95ff5673c22
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-10-02 21:11:48 +0600

    Custom tailwind class and font setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+
 const Home = () => {
   return (
     <div>
-      <h2 className="text-3xl">this is home</h2>
+      <Header></Header>
+      <Navbar></Navbar>
+      <h2 className="text-3xl font-poppins font-bold">this is home</h2>
     </div>
   );
 };
---
+++ src/pages/Shared/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">This is header</h2>
+    </div>
+  );
+};
+
+export default Header;
