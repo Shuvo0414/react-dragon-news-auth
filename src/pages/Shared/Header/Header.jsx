57d848164c5c9a43ee871fddc66146af0031df0f
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-10-02 21:36:43 +0600

    Homepage layout and date time formatting using moment js

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,16 @@
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+
 const Header = () => {
   return (
     <div>
-      <h2 className="text-2xl">This is header</h2>
+      <img src={logo} alt="logo png" />
+      <p className=" text-base font-normal">
+        Journalism Without Fear or Favour
+      </p>
+      <p className=" text-xl font-medium">
+        {moment().format("dddd, MMMM D, YYYY")}
+      </p>
     </div>
   );
 };
